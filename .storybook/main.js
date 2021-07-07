a2cef5ce99aee8abceee5a374035ff859bb165c8
Alexey Ekimov <AEkimov2@slb.com>
2021-07-07 14:24:26 -0500

    SB v6.4.0-alpha.12

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
       '@storybook/addon-docs',
       '@storybook/addon-actions',
       '@storybook/addon-links',
-      '@storybook/addon-knobs',
       '@storybook/addon-a11y',
       '@storybook/addon-viewport',
   ],
